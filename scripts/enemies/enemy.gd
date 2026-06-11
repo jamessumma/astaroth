bca67dd8c5b928db6767d018accad5857454897f
James Summa <jamessumma@proton.me>
2026-06-11 16:31:20 -0400

    updated enemy fields

--- scripts/enemies/enemy.gd
+++ scripts/enemies/enemy.gd
@@ -1,9 +1,13 @@
 extends Node
 
 class_name Enemy
-
+# all enemies have health, awareness of the player, and attacks
+# different variants will override these attributes and methods
 @export var max_health: int = 100
 var current_health: int
+
+var aware_of_player = false
+var line_of_sight_player = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
