3ce2836110b7948b7544ca59fbef2d9f68d7cda1
jamessumma <jamessumma@pm.me>
2026-06-22 09:10:14 -0400

    adding new sewer pieces

--- scripts/globals/game_manager.gd
+++ scripts/globals/game_manager.gd
@@ -1,12 +1,32 @@
 extends Node
 
+# singleton player
 var player = null
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# settings
+var sfx_volume: float = 100.0
+var music_volume: float = 100.0
+var master_volume: float = 100.0
 
+func _ready():
+	apply_settings()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func set_sfx_volume(value: float):
+	sfx_volume = clamp(value, 0.0, 100.0)
+	apply_settings()
+	
+func set_music_volume(value: float):
+	music_volume = clamp(value, 0.0, 100.0)
+	apply_settings()
+	
+func set_master_volume(value: float):
+	master_volume = clamp(value, 0.0, 100.0)
+	apply_settings()
+	
+func apply_settings():
+	var sfx_bus = AudioServer.get_bus_index("SFX")
+	var music_bus = AudioServer.get_bus_index("Music")
+	var master_bus = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(sfx_volume / 100.0))
+	AudioServer.set_bus_volume_db(music_bus, linear_to_db(music_volume / 100.0))
+	AudioServer.set_bus_volume_db(master_bus, linear_to_db(master_volume / 100.0))
