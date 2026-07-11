196fc7adfcb037e936e753095a738add9d3ab9d1
jamessumma <jamessumma@pm.me>
2026-07-11 15:06:41 -0400

    updated file locations and added base attack resource

---
+++ scripts/enemies/attack.gd
@@ -0,0 +1,11 @@
+class_name Attack
+extends Resource
+
+@export var attack_name: String
+@export var min_range: float
+@export var max_range: float
+@export var base_weight: float = 1.0
+@export var cur_weight: float = 1.0
+@export var base_damage: float = 1.0
+@export var knockback: bool = false
+
