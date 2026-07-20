761bc3fa12627a1620f6d2111f7ee2b71e59abb1
jamessumma <jamessumma@pm.me>
2026-07-19 21:23:18 -0400

    added cheese attacks

---
+++ scripts/enemies/mr_cheese/attack_cheese_punch.gd
@@ -0,0 +1,13 @@
+class_name AttackCheesePunch
+
+extends Attack
+
+func _init():
+  attack_name = "Lead Jab|mixamo_com"
+  attack_duration = 0.75
+  min_range = 0.0
+  max_range = 2.0
+  base_weight = 1.0
+  cur_weight = 1.0
+  base_damage = 1.0
+  knockback = true
---
+++ scripts/enemies/mr_cheese/attack_cheese_throw.gd
@@ -0,0 +1,13 @@
+class_name AttackCheeseFlyingKick
+
+extends Attack
+
+func _init():
+  attack_name = "Throw Object|mixamo_com"
+  attack_duration = 0.75
+  min_range = 1.0
+  max_range = 10.0
+  base_weight = 1.0
+  cur_weight = 1.0
+  base_damage = 1.0
+  knockback = true
