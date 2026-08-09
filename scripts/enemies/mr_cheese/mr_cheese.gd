6a0bffd0bff6b90f1f09e8342066854dabfd6e2f
jamessumma <jamessumma@pm.me>
2026-08-08 20:17:13 -0400

    cleaned comments

--- scripts/enemies/mr_cheese/mr_cheese.gd
+++ scripts/enemies/mr_cheese/mr_cheese.gd
@@ -16,12 +16,6 @@ func _ready() -> void:
 	self.attacks = [AttackCheesePunch.new(), AttackCheeseThrow.new(), AttackCheeseFlyingKick.new(), AttackCheeseJump.new()]
 	animation_player.animation_finished.connect(_on_animation_finished)
 
-#func _physics_process(delta: float) -> void:
-#	attack_timer_cur = clamp(attack_timer_cur - delta, 0, attack_timer_max)
-#	var rm := animation_player.get_root_motion_position()
-#	velocity = (transform.basis * rm) / delta
-#	enemy_process(delta)
-
 func _physics_process(delta: float) -> void:
 	attack_timer_cur = clamp(attack_timer_cur - delta, 0, attack_timer_max)
 
@@ -53,7 +47,7 @@ func handle_attack(delta: float):
 
 func enter_exec_attack():
 	print(self.next_attack.attack_name)
-	animation_player.play(self.next_attack.attack_name, 0.6)
+	animation_player.play(self.next_attack.attack_name, 0.6, self.next_attack.speed_mod)
 	print("post attack")
 	attack_timer_cur = attack_timer_max
 
@@ -70,4 +64,3 @@ func _on_animation_finished(anim_name):
 	if next_attack:
 		if anim_name == next_attack.attack_name:
 			self.state_machine.trigger_event(Events.type.ATTACK_FINISHED)
-			#animation_player.root_motion_track = NodePath()
